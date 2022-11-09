99356e9d373fbd43f0ee5f656bc8d0baf77302bb
Augustyński Iwo <Iwo.Augustynski@IOS.local>
2022-11-09 20:29:40 +0100

    .

---
+++ R_Graphics/GGPlot2_Part3/customTests.R
@@ -0,0 +1,107 @@
+# Put custom tests in this file.
+      
+      # Uncommenting the following line of code will disable
+      # auto-detection of new variables and thus prevent swirl from
+      # executing every command twice, which can slow things down.
+      
+      # AUTO_DETECT_NEWVAR <- FALSE
+      
+      # However, this means that you should detect user-created
+      # variables when appropriate. The answer test, creates_new_var()
+      # can be used for for the purpose, but it also re-evaluates the
+      # expression which the user entered, so care must be taken.
+
+# So swirl does not repeat execution of plot commands
+AUTO_DETECT_NEWVAR <- FALSE
+
+# Check that the output/value produced by a script is correct
+script_vals_identical <- function() {
+  # Get e
+  e <- get('e', parent.frame())
+  # Get value produced from user's script
+  user_val <- capture.output(
+    local(
+      try(
+        # Must use eval-parse combo, not source, if we don't force user
+        # to print result
+        eval(e$expr),
+        silent = TRUE
+      )
+    )
+  )
+  # Get value produced from correct script
+  correct_val <- capture.output(
+    local(
+      try(
+        # Must use eval-parse combo, not source, if we don't force user
+        # to print result
+        eval(parse(file = e$correct_script_temp_path)),
+        silent = TRUE
+      )
+    )
+  )
+  # Compare values
+  identical(user_val, correct_val)
+}
+
+# Returns TRUE if e$expr matches any of the expressions given
+# (as characters) in the argument.
+ANY_of_exprs <- function(...){
+  e <- get("e", parent.frame())
+  any(sapply(c(...), function(expr)omnitest(expr)))
+}
+
+equiv_val <- function(correctVal){
+  e <- get("e", parent.frame()) 
+  #print(paste("User val is ",e$val,"Correct ans is ",correctVal))
+  isTRUE(all.equal(correctVal,e$val))
+  
+}
+
+# Stage a variable for inclusion is swirl's protected list. Staged variables
+# will be included if and only if a user's answer passes all the current tests.
+stageVariable <- function(name_of_variable, value){
+  name_of_variable <- as.character(name_of_variable)
+  e <- getState()
+  e$delta[[name_of_variable]] <- value
+}
+
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Get the value which a user either entered directly or was computed
+# by the command he or she entered.
+getVal <- function(){
+  getState()$val
+}
+
+# Get the last expression which the user entered at the R console.
+getExpr <- function(){
+  getState()$expr
+}
+
+save_credit <- function(){
+  e <- get("e", parent.frame())
+  if(e$val == "No") return(TRUE)
+  if(e$val == "Yes"){
+    name <- readline("What is your full name? ")
+    id <- readline("What is your student's number? ")
+    # Get course and lesson names
+    course_name <- attr(e$les, "course_name")
+    lesson_name <- attr(e$les, "lesson_name")
+    
+    if (file.exists("results.RDS")) {
+      tmp <- readRDS('results.RDS')
+      new <- data.frame(name = name, id = id, lesson = lesson_name, course = course_name, time = Sys.time())
+      tmp <- rbind(tmp,new)
+    } else {
+      tmp <- data.frame(name = name, id = id, lesson = lesson_name, course = course_name, time = Sys.time()) 
+    }
+    saveRDS(tmp, "results.RDS")
+  }
+  TRUE
+}
